d1fdbcbbc613a6811a88cd95963a0e233f1eb937
Gabriela Araujo <gabyara237@gmail.com>
2024-10-28 21:00:29 -0700

    Set Up User Input and Output

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class GuessingGame {
     public static void main(String[] args) {
 
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("Enter an item: ");
+        String itemInput = scanner.nextLine();
+        System.out.printf("Your answer was: %s", itemInput);
     }
 }
