45d4b8fdee327686957d5e1fa195418b408deb92
Gabriela Araujo <gabyara237@gmail.com>
2024-10-30 11:58:55 -0700

    Implement a while loop in the game for player guesses with feedback and counter.

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class GuessingGame {
     public static void main(String[] args) {
 
+        int guessAttemptCounter = 0;
         Scanner scanner =  new Scanner(System.in);
         System.out.println("What type of item should fill the jar? ");
         String itemInput = scanner.nextLine();
@@ -11,9 +12,27 @@ public class GuessingGame {
         int maximumNumberInput = scanner.nextInt();
 
         Jar jar = new Jar(itemInput,maximumNumberInput);
-        
+
         jar.fill();
-        System.out.printf("How many %s are in the jar? Pick a number between 1 and %d. %n", itemInput ,maximumNumberInput);
-        int userGuessedNumber = scanner.nextInt();
+
+        int userGuessedNumber=0;
+        int currentNumberItems = jar.getCurrentNumberItems();
+
+        while( userGuessedNumber != currentNumberItems ){
+            System.out.printf("How many %s are in the jar? Pick a number between 1 and %d. %n", itemInput ,maximumNumberInput);
+            userGuessedNumber = scanner.nextInt();
+            if( userGuessedNumber == currentNumberItems){
+                break;
+            }
+            if( userGuessedNumber < currentNumberItems){
+                System.out.printf("Your number is lower than the actual number of %s %n", itemInput);
+            }else{
+                System.out.printf("Your number is higher than the actual number of %s %n", itemInput);
+            }
+
+            guessAttemptCounter++;
+
+        }
+        
     }
 }
