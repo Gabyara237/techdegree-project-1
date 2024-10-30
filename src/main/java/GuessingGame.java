c2b131e51a430768104584dab7483426395ef41a
Gabriela Araujo <gabyara237@gmail.com>
2024-10-30 14:07:48 -0700

    Add comments.

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,29 +1,34 @@
 public class GuessingGame {
     public static void main(String[] args) {
 
+        // New instance of the Prompter class to handle user prompts and input.
         Prompter prompter = new Prompter();
 
         int guessAttemptCounter = 0;
         
         String itemInput = prompter.itemInput();
-
         int maximumNumberInput = prompter.maximumNumberInput();
-
+        
+        // New instance of the Jar class.
         Jar jar = new Jar(itemInput,maximumNumberInput);
-
+        
+        // The method is called to fill the jar with a random number of items.
         jar.fill();
 
         int userGuessedNumber=0;
+        
+        // The current number of elements in the jar is obtained.
         int currentNumberItems = jar.getCurrentNumberItems();
 
+        // While allowing the player to enter his guess until he guesses the correct number of items in the jar.
         while( userGuessedNumber != currentNumberItems ){
-
+            
             try{
                 userGuessedNumber = prompter.userGuessedNumber(currentNumberItems);
                 if( userGuessedNumber == currentNumberItems){
                     break;
                 }
-
+                // Feedback is provided based on the number entered by the user.
                 prompter.feedback(userGuessedNumber < currentNumberItems);
                 guessAttemptCounter++;
 
@@ -32,6 +37,7 @@ public class GuessingGame {
             }
             
         }
+        // Display results once the player guesses the number of items in the jar.
         prompter.displayScore(guessAttemptCounter);
         
     }
--- src/main/java/Prompter.java
+++ src/main/java/Prompter.java
@@ -1,46 +1,53 @@
 import java.util.Scanner;
 
 public class Prompter{
+
     private String itemInput;
     private int maximumNumberInput;
     private int userGuessedNumber;
     Scanner scanner =  new Scanner(System.in);
 
+    // Method requesting type of items
     public String itemInput(){
         
         System.out.println("What type of item should fill the jar? ");
         itemInput = scanner.nextLine();
         return itemInput;
     }
-
+    
+    // Method requesting item quantity
     public int maximumNumberInput(){
         System.out.printf("What is the maximum amount of %s: %n", itemInput);
         maximumNumberInput = scanner.nextInt();
         return maximumNumberInput;
     }
-
+    
+    // Method that prompts the user to enter the amount of items he/she believes are in the jar.
     public int userGuessedNumber(int currentNumberItems){
         System.out.printf("How many %s are in the jar? Pick a number between 1 and %d. %n", itemInput ,maximumNumberInput);
         userGuessedNumber = scanner.nextInt();
         if(userGuessedNumber > maximumNumberInput){
-            throw new IllegalArgumentException(String.format(" Your guess must be less than %d ", maximumNumberInput));
+            throw new IllegalArgumentException("Your guess must be less than maximum fill amount");
         }
         return userGuessedNumber;
     }
 
+    // Method that offers the user a feeback according to the number entered and the number to guess.
     public void feedback(boolean lessThan){
 
         if (lessThan){
-            System.out.printf("Your number is lower than the actual number of %s %n", itemInput);
+            System.out.println("Your guess is too low.");
         }else{
-            System.out.printf("Your number is higher than the actual number of %s %n", itemInput);
+            System.out.println("Your guess is too high.");
         }
     }
 
+    // Method that display result after user wins the game
     public void displayScore(int count){
         System.out.printf("You got it in %d attempt(s)! ", count + 1);
     }
-
+    
+    // Method that displays the error message
     public void displayIaeMessage(String message){
         System.out.printf("%s please try again. %n", message);
     }
